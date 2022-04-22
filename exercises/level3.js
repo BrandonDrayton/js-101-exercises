ff12a30b2abc292b25316d1f245f8709701e4082
Brandon Drayton <brandondrayton@gmail.com>
2022-04-22 16:49:51 -0400

    Finished levels 3 and 4

--- exercises/level3.js
+++ exercises/level3.js
@@ -1,24 +1,34 @@
-function lvl3exercise1 () {
+function lvl3exercise1() {
   // Create strings "hello" and a "world", return the concatenation of the two
-
+  variable1 = 'hello'
+  variable2 = 'world'
+  return variable1 + variable2
 }
 
-function lvl3exercise2 () {
+function lvl3exercise2() {
   // Create a "hello" and a 12, return the concatenation of the two
-
+  variable1 = 'hello'
+  variable2 = 12
+  return variable1 + variable2
 }
 
-function lvl3exercise3 () {
+function lvl3exercise3() {
   // Create a variable that equals 12 and convert it to a string with concatenation. Return it.
-
+  variable = 12
+  variable2 = String(variable)
+  return variable2
 }
 
-function lvl3exercise4 () {
+function lvl3exercise4() {
   // Create a "hello world!" string. Return the length of the string
-
+  variable = 'hello world!'
+  variable2 = variable.length
+  return variable2
 }
 
-function lvl3exercise5 () {
+function lvl3exercise5() {
   // Create a "hello world!" string. Return the index of the word "world".
-
+  variable = 'hello world'
+  variable2 = variable.indexOf('world')
+  return variable2
 }
